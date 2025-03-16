7583d991d36ef64b4fdaaa9815c24aa4784110c3
Vincent <140604446+vincenzo1109@users.noreply.github.com>
2025-03-16 08:32:11 +0100

    a

--- Save Kopie vom USB-Stick/Python/Lotto.py
+++
@@ -1,6 +0,0 @@
-import random
-alle_Lottozahlen = []
-alle_Lottozahlen.extend(range(1,50))
-Lottozahlen_Gewinner = random.sample(alle_Lottozahlen, 6)
-Lottozahlen_Gewinner.sort()
-print(Lottozahlen_Gewinner)

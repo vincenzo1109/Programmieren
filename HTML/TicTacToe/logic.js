c8a2cb54371a6be995ecaecdf9a3bd5cbd971f38
Vincent <140604446+vincenzo1109@users.noreply.github.com>
2025-04-11 22:54:05 +0200

    a

--- HTML/TicTacToe/logic.js
+++ HTML/TicTacToe/logic.js
@@ -1,8 +1,21 @@
+let currentplayer = 'X'
+
 function auslesen() {
 
     let number1 = document.getElementById('number1').value, number2 = document.getElementById('number2').value;
-    let n1= parseFloat(number1);
-    let n2= parseFloat(number2);
+    let n1 = parseFloat(number1);
+    let n2 = parseFloat(number2);
     let Ergebnis = n1 + n2;
-document.getElementById('Output1').innerText = 'Ergebnis: ' + Ergebnis.toString();
+    document.getElementById('Output1').innerText = 'Ergebnis: ' + Ergebnis.toString();
+}
+
+function Zeichnen(a) {
+    let pressedBox = document.getElementById(a);
+
+    pressedBox.innerText = currentplayer;
+    if (currentplayer === 'X') {
+        currentplayer = 'O'
+    } else {
+        currentplayer = 'X'
+    }
 }

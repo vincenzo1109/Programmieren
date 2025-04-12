d8b9d6ce75b8962636aef80e1b1318e8e8b61b22
Vincent <140604446+vincenzo1109@users.noreply.github.com>
2025-04-12 23:19:52 +0200

    a

--- HTML/TicTacToe/logic.js
+++ HTML/TicTacToe/logic.js
@@ -11,7 +11,7 @@ function auslesen() {
 
 function Zeichnen(a) {
     let pressedBox = document.getElementById(a);
-    if (pressedBox.innerText == 'X' || pressedBox.innerText == 'O') {
+    if (pressedBox.innerText === 'X' || pressedBox.innerText === 'O') {
         alert('Dieses Feld ist bereits belegt')
     } else {
         pressedBox.innerText = currentplayer;
@@ -21,31 +21,35 @@ function Zeichnen(a) {
             currentplayer = 'X'
         }
     }
-    Board_Kopieren()
+    let Player1 = document.getElementById("player1").value;
+    let Player2 = document.getElementById("player2").value;
+    console.log(Player1, Player2);
+
+    Board_Kopieren(Player1, Player2)
 }
 
-function Board_Kopieren() {
+function Board_Kopieren(Player1, Player2) {
     let fields = []
     for (let i = 0; i < 9; i++) {
         let field = document.getElementById(i).innerText;
         fields.push(field);
     }
     console.log(fields);
-    Gewonnen(fields);
-    Unentschieden(fields)
+    Gewonnen(fields, Player1, Player2);
+    Unentschieden(fields);
 }
 
-function Gewonnen(Board) {
+function Gewonnen(Board, Player1, Player2) {
     for (let i = 0; i < 9; i++) {
 //Zeilenprüfung
         if (Board[3 * i] === Board[i * 3 + 1] && Board[i * 3 + 1] === Board[i * 3 + 2] && Board [i * 3] !== '') {
             setTimeout(function Timer() {
                 if (Board[i * 3] === 'X') {
-                    alert('X     hat gewonnen')
+                    Points(Player1)
                     Reset(Board)
 
                 } else if (Board[i * 3] === 'O') {
-                    alert('O hat gewonnen')
+                    Points(Player2)
                     Reset(Board)
                 }
             }, 150)
@@ -61,7 +65,7 @@ function Reset(Board) {
 }
 
 function Unentschieden(board) {
-    setTimeout(() =>{
+    setTimeout(() => {
         let a = 0
         for (let i = 0; i < 9; i++) {
             if (board[i] !== '') {
@@ -69,9 +73,14 @@ function Unentschieden(board) {
                 console.log(a)
             }
         }
-        if (a === 9){
+        if (a === 9) {
             alert('Es ist ein Unentschieden')
-            Reset(board)}
+            Reset(board)
+        }
     }, 150)
+}
 
+function Points(Player) {
+    console.log('Gewinner: ' + Player);
+    document.getElementById("Points").innerText = Player + ' hat gewonnen'
 }

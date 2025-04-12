f6b0e03515e02de1266d7bdb6720e7da48fd5327
Vincent <140604446+vincenzo1109@users.noreply.github.com>
2025-04-12 22:12:26 +0200

    a

--- HTML/TicTacToe/logic.js
+++ HTML/TicTacToe/logic.js
@@ -21,4 +21,57 @@ function Zeichnen(a) {
             currentplayer = 'X'
         }
     }
+    Board_Kopieren()
+}
+
+function Board_Kopieren() {
+    let fields = []
+    for (let i = 0; i < 9; i++) {
+        let field = document.getElementById(i).innerText;
+        fields.push(field);
+    }
+    console.log(fields);
+    Gewonnen(fields);
+    Unentschieden(fields)
+}
+
+function Gewonnen(Board) {
+    for (let i = 0; i < 9; i++) {
+//Zeilenprüfung
+        if (Board[3 * i] === Board[i * 3 + 1] && Board[i * 3 + 1] === Board[i * 3 + 2] && Board [i * 3] !== '') {
+            setTimeout(function Timer() {
+                if (Board[i * 3] === 'X') {
+                    alert('X     hat gewonnen')
+                    Reset(Board)
+
+                } else if (Board[i * 3] === 'O') {
+                    alert('O hat gewonnen')
+                    Reset(Board)
+                }
+            }, 150)
+        }
+    }
+}
+
+function Reset(Board) {
+    for (let j = 0; j < 9; j++) {
+        Board[j] = ''
+        document.getElementById(j).innerText = Board[j]
+    }
+}
+
+function Unentschieden(board) {
+    setTimeout(() =>{
+        let a = 0
+        for (let i = 0; i < 9; i++) {
+            if (board[i] !== '') {
+                a = a + 1
+                console.log(a)
+            }
+        }
+        if (a === 9){
+            alert('Es ist ein Unentschieden')
+            Reset(board)}
+    }, 150)
+
 }
